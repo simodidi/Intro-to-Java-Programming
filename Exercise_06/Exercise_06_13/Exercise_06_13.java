0b606917c60f43be67c5dff71ce929b3e22fc5de
simodidi <simodidi267@gmail.com>
2025-04-24 15:58:28 +0800

    Update Exercise_06_13.java

--- Exercise_06/Exercise_06_13/Exercise_06_13.java
+++ Exercise_06/Exercise_06_13/Exercise_06_13.java
@@ -1,42 +1,20 @@
 /*
 (Sum series) Write a method to compute the following series:
 
-	m(i) = 1 / 2 + 2 / 3 + ... + i / (i + 1)
+	m(i) = 1 / 3 +  2/ 4 + ... + i / (i + 2)
 
 Write a test program that displays the following table:
 
 i       m(i)
 --------------
-1        0.5000
-2        1.1667
-...
-19       16.4023
-20       17.3546
-*/
-public class Exercise_06_13 {
-	/** Main Method */
-	public static void main(String[] args) {
-		// Display table series data between 1.0 t0 20.0
-		double seriesStart  =  1.0;	// Start range
-		double numberOfRows = 20.0;	// End range
-
 		// Display header
 		System.out.println("\ni         m(i)     ");
 		System.out.println("-------------------");
 
-		// Display Data
-		for (double i = seriesStart; i <= numberOfRows; i++) {
-			System.out.printf("%-10.0f", i);
-			System.out.printf("%7.4f\n", sumSeries(i));
-		}
-	}
+1        0.3333
+2       0.8333
+...
+19       14.7093
+20       15.6184
+*/
 
-	/*** Method sumSeries: computes the series */
-	public static double sumSeries(double n) {
-		double sum = 0;	// Sum of the series
-		for (double i = 1; i <= n; i++) {
-			sum += i / (i + 1);
-		}
-		return sum;
-	}
-}
